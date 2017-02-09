e97bfcbc73493322906912b2bf5745f6da0f922f
student <Harrychaplain@hotmail.co.uk>
2017-02-09 16:06:14 +0000

    blank data handler classes

---
+++ agriculture/src/agriculture/MaxMinDataHandler.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agriculture;
+
+/**
+ *
+ * @author student
+ */
+public class MaxMinDataHandler {
+    private double MaxReading;
+    private double MinReading;
+    
+    public Reading handleRawData(){
+        
+        return 0;
+        
+    }
+}
