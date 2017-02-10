bba40e142f99ae27112a391bc9342559c0dffa4b
student <Harrychaplain@hotmail.co.uk>
2017-02-10 15:42:48 +0000

    Field and plots NOT FINISHED

---
+++ agriculture/src/agriculture/SetOfPlots.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agriculture;
+
+/**
+ *
+ * @author student
+ */
+public class SetOfPlots {
+    
+    public SetOfPlots()
+    {
+        super();
+    }
+    
+    public void addPlot(Plot aPlot){
+        super.add(aPlot);
+    }
+    
+}
