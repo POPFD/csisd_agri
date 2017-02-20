5b1178b53f44980db894fa38b46ed651fe441e47
JackAllister <b3042098@my.shu.ac.uk>
2017-02-20 14:13:39 +0000

    Added skeleton for clock, added SensorMonitor class. Need to create a SetOfReadings class and internals for clock

---
+++ agriculture/src/agriculture/Clock.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agriculture;
+
+/**
+ *
+ * @author user
+ */
+public class Clock {
+    
+    void tick() {
+        /* Not sure what this does yet */
+    }
+    
+    
+    void notifyMonitor() {
+        
+    }
+    
+    void attach(SensorMonitor sensMon) {
+        
+    }
+    
+    void deattach(SensorMonitor sensMon) {
+        
+    }
+    
+    
+    
+}
---
+++ agriculture/src/agriculture/SensorMonitor.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agriculture;
+
+import java.util.Date;
+
+/**
+ *
+ * @author user
+ */
+public class SensorMonitor {
+    
+    private Sensor sensor;
+    private SetOfReadings readings;
+    private double readingFrequency;
+    private Date lastReading;
+    
+    
+    public SensorMonitor(double frequency, Sensor sens) {
+        this.sensor = sens;
+        this.readingFrequency = frequency;
+        
+        this.readings = new SetOfReadings();       
+    }
+    
+    
+    /* No need to have takeReadingIfRequired as that function did nothing else */
+    public void requestReading() {
+        Reading processedReading = sensor.getReading();
+        
+        /* 
+         * No need for updateReadings as it would be
+         * a one line function otherwise. We may as well 
+         * add directly to SetOfReadings.
+         */
+        readings.add(processedReading);
+    }
+    
+    /* Was postReadings in diagram, changed to be more explanatory */
+    public SetOfReadings getReadings() {
+        return readings;
+    }
+    
+}
