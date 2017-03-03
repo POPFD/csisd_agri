3dba7bccd785881bf7e23d13517cd94b2e44b805
JackAllister <b3042098@my.shu.ac.uk>
2017-03-03 13:56:34 +0000

    Cumulative handler resets after x amount of ticks

--- agriculture/src/agriculture/CumulativeDataHandler.java
+++ agriculture/src/agriculture/CumulativeDataHandler.java
@@ -12,15 +12,25 @@ package agriculture;
 public class CumulativeDataHandler extends DataHandlerMethod {
     
     private Double cumulativeTotal = 0.0;
+    private int tickCount = 0;
     
     @Override
-    public Reading handleRawData(Reading rawReading){
+    public Reading handleRawData(Reading rawReading) {
+        
+        /* To simulate cumulative resetting after set amount of time */
+        if (tickCount >= 24)
+        {
+            cumulativeTotal = 0.0;
+            tickCount = 0;
+        }
+        
         cumulativeTotal += (Double)rawReading.getReadingValue();
         
         Reading procReading = new Reading(rawReading.getReadingTime(),
                                             cumulativeTotal,
                                             rawReading.getReadingLocation());
         
+        tickCount++;
         return procReading;
         
     }
