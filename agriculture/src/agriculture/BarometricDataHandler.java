82a95c5fbb0f58ba2ed7492ff0140cb41d4ecad0
JackAllister <b3042098@my.shu.ac.uk>
2017-03-03 12:22:33 +0000

    Removed discrete monitor and finished barometric handler

--- agriculture/src/agriculture/BarometricDataHandler.java
+++ agriculture/src/agriculture/BarometricDataHandler.java
@@ -11,12 +11,27 @@ package agriculture;
  */
 public class BarometricDataHandler extends DataHandlerMethod{
     
-    private Reading previousReading;
+    private Reading previousReading = null;
     
+    @Override
     public Reading handleRawData(Reading rawReading){
         
-        return 0;
         
+        double readingVal = rawReading.getReadingValue();
+        if (previousReading != null)
+        {
+            /* Take average of last reading and current */
+            readingVal += previousReading.getReadingValue();
+            readingVal /= 2;
+        }        
+        
+        Reading procReading = new Reading(rawReading.getReadingTime(), 
+                                        readingVal, 
+                                        rawReading.getReadingLocation());
+              
+        /* Save last reading for next use */
+        previousReading = rawReading;
+        return procReading;       
     }
     
 }
